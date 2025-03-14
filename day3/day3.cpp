06531b727346883e35d6741248133cc7936ec41d
LucaB03 <luca.blase10@gmail.com>
2025-03-14 20:29:08 +0100

    day 3 - 2/2

--- day3/day3.cpp
+++ day3/day3.cpp
@@ -1,78 +1,40 @@
 #include <fstream>
 #include <iostream>
+#include <regex>
+#include <iterator>
+#include <vector>
 
 using namespace std;
 
-int count(ifstream * stream);
+int sum(ifstream * stream);
 
 int main() {
     ifstream file("input.txt");
-    cout << "The sum of the mul-operations is " << count(&file);
+    cout << "The sum of the mul-operations is " << sum(&file);
     return 0;
 }
 
-//simple DFA implementation for mul-regex
-int count(ifstream * stream) {
+//using regex
+//Part 2 included
+int sum(ifstream * stream) {
+    int sum = 0;
+    bool enabled = true;
+    //buffer the whole file into a vector
+    vector<char> buffer;
     char c = stream->get();
-    int mulOpParts = 0, mulOpCounter = 0;
-    int x = 0, y = 0, sum = 0;
-    while (c != EOF) {
-        if (mulOpParts == 8) {
-            ++mulOpCounter;
-            mulOpParts = 0;
-            sum += x * y;
-            x = 0;
-            y = 0;
-        } 
-        if (mulOpParts == 0 && c == 'm') ++mulOpParts;
-        else if (mulOpParts == 1 && c == 'u') ++mulOpParts;
-        else if (mulOpParts == 2 && c == 'l') ++mulOpParts;
-        else if (mulOpParts == 3 && c == '(') ++mulOpParts;
-        else if (mulOpParts == 4 && (c >= 48 && c <= 57)) {
-            ++mulOpParts;
-            int digitCounter = 1;
-            string digits = "";
-            while (digitCounter < 4) {
-                digits += c;
-                char p = stream->peek();
-                if (p >= 48 && p <= 57) {
-                    ++digitCounter;
-                    c = stream->get();
-                }
-                else if (p == ',') break;
-                else {
-                    mulOpParts = 0;
-                    break;
-                }
-            }
-            if (digitCounter > 3) mulOpParts = 0;  
-            x = stoi(digits);          
-        }
-        else if (mulOpParts == 5 && c == ',') ++mulOpParts;
-        else if (mulOpParts == 6 && (c >= 48 && c <= 57)) {
-            ++mulOpParts;
-            int digitCounter = 1;
-            string digits = "";
-            while (digitCounter < 4) {
-                digits += c;
-                char p = stream->peek();
-                if (p >= 48 && p <= 57) {
-                    ++digitCounter;
-                    c = stream->get();
-                }
-                else if (p == ')') break;
-                else {
-                    mulOpParts = 0;
-                    break;
-                }
-            }
-            if (digitCounter > 3) mulOpParts = 0;           
-            y = stoi(digits);    
-        }
-        else if (mulOpParts == 7 && c == ')') ++mulOpParts;
-        else mulOpParts = 0; 
+    while(c != EOF) {
+        buffer.push_back(c);
         c = stream->get();
     }
-    
+    string s(buffer.begin(), buffer.end());
+
+    regex mul("mul\\((\\d{1,3}),(\\d{1,3})\\)|do\\(\\)|don't\\(\\)");
+    auto itBegin = sregex_iterator(s.begin(), s.end(), mul);
+    auto itEnd = sregex_iterator();
+    for (regex_iterator i = itBegin; i != itEnd; ++i) {
+        if (i->str() == "do()" || i->str() == "don't()") enabled = i->str() == "do()" ? true : false;
+        else if (enabled) sum += stoi((*i)[1]) * stoi((*i)[2]);
+    }
+    stream->close();
     return sum;
 }
